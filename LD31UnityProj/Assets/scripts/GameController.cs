b22134500cf0d23b2960a33db887f4851b1b00bb
Rob Hailman <robhailman@gmail.com>
2014-12-06 02:22:16 -0500

    First steps
    Add: Game controller
    Add: Map controller
    Add: Tile laying feature
    Add: Placeholder sprites for trench tiles and screen overlay
    Add: todo list

---
+++ LD31UnityProj/Assets/scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    public Camera LevelCamera;
+    public GameObject SelectionBox;
+    public int MapX = 34;
+    public int MapY = 34;
+
+    private MapController mapController;
+
+    // Use this for initialization
+    void Start()
+    {
+        mapController = GameObject.Find("map").GetComponent<MapController>();
+        mapController.MapX = MapX;
+        mapController.MapY = MapY;
+        mapController.TileArray = new Tiles[MapX, MapY];
+        mapController.GameObjectArray = new GameObject[MapX, MapY];
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 p = LevelCamera.ScreenToWorldPoint(Input.mousePosition);
+        p.x = Mathf.Floor(p.x);
+        p.y = Mathf.Floor(p.y);
+        if (p.x < 0 | p.x >= MapX | p.y < 0 | p.y >= MapY)
+        {
+            SelectionBox.SetActive(false);
+        } else
+        {
+            SelectionBox.SetActive(true);
+            SelectionBox.transform.position = p;
+        }
+
+        if (Input.GetMouseButton(0) & SelectionBox.activeSelf)
+        {
+            mapController.PlaceTile(p);
+        }
+
+        if (Input.GetMouseButton(1) & SelectionBox.activeSelf)
+        {
+            mapController.RemoveTile(p);
+        }
+    }
+}
---
+++ LD31UnityProj/Assets/scripts/MapController.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Tiles
+{
+    Empty,
+    Vert,
+    Horiz,
+    T_Vert_L,
+    T_Vert_R,
+    T_Horiz_D,
+    T_Horiz_U,
+    X,
+    Point,
+    L_L_U,
+    L_R_U,
+    L_L_D,
+    L_R_D
+}
+
+public class MapController : MonoBehaviour
+{
+    public int MapX;
+    public int MapY;
+    public Tiles[,] TileArray;
+    public GameObject[,] GameObjectArray;
+    public Transform MapTransform;
+    public GameObject[] TileSprites;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public bool PlaceTile(Vector2 location)
+    {
+        Debug.Log("Placing tile at " + location);
+        int x = (int)location.x;
+        int y = (int)location.y;
+
+        // check if tile is empty
+        if (TileArray[x, y] != Tiles.Empty)
+        {
+            Debug.Log("Tile is not empty.");
+            return false;
+        }
+
+        if (x == 0 | x == MapX - 1 | y == 0 | y == MapY - 1)
+        {
+            Debug.Log("Can't place tile on border!");
+            return false;
+        }
+
+        TileArray[x, y] = CheckNeighbours(x, y);
+        UpdateTileObject(x, y);
+
+        RefreshTile(x - 1, y);
+        RefreshTile(x + 1, y);
+        RefreshTile(x, y - 1);
+        RefreshTile(x, y + 1);
+
+        return true;
+    }
+
+    public bool RemoveTile(Vector2 location)
+    {
+        int x = (int)location.x;
+        int y = (int)location.y;
+        
+        // check if tile is empty
+        if (TileArray[x, y] == Tiles.Empty)
+        {
+            Debug.Log("Tile is empty.");
+            return false;
+        }
+
+        if (x == 0 | x == MapX - 1 | y == 0 | y == MapY - 1)
+        {
+            Debug.Log("Can't place tile on border!");
+            return false;
+        }
+
+        TileArray[x, y] = Tiles.Empty;
+        Destroy(GameObjectArray[x, y]);
+        GameObjectArray[x, y] = null;
+
+        RefreshTile(x - 1, y);
+        RefreshTile(x + 1, y);
+        RefreshTile(x, y - 1);
+        RefreshTile(x, y + 1);
+
+        return true;
+    }
+
+    private void RefreshTile(int x, int y)
+    {
+        if (TileArray[x, y] != Tiles.Empty)
+        {
+            Tiles oldTile = TileArray[x, y];
+            TileArray[x, y] = CheckNeighbours(x, y);
+            if (TileArray[x, y] != oldTile)
+            {
+                UpdateTileObject(x, y);
+            }
+        }
+    }
+
+    private void UpdateTileObject(int x, int y)
+    {
+        Destroy(GameObjectArray[x, y]);
+        GameObject newObject = (GameObject)Instantiate(TileSprites[(int)TileArray[x, y]], new Vector3(x, y), 
+                                                       Quaternion.identity);
+
+        Transform newTransform = newObject.GetComponent<Transform>();
+        newTransform.parent = MapTransform;
+        GameObjectArray[x, y] = newObject;
+    }
+
+    private Tiles CheckNeighbours(int x, int y)
+    {
+        if (TileArray[x - 1, y] == Tiles.Empty & TileArray[x + 1, y] == Tiles.Empty 
+            & (TileArray[x, y - 1] != Tiles.Empty | TileArray[x, y + 1] != Tiles.Empty))
+        {
+            return Tiles.Vert;
+        }
+        // horiz
+        else if ((TileArray[x - 1, y] != Tiles.Empty | TileArray[x + 1, y] != Tiles.Empty) 
+            & TileArray[x, y - 1] == Tiles.Empty & TileArray[x, y + 1] == Tiles.Empty)
+        {
+            return Tiles.Horiz;
+        }
+        // T_Vert_L
+        else if (TileArray[x - 1, y] != Tiles.Empty & TileArray[x + 1, y] == Tiles.Empty 
+            & TileArray[x, y - 1] != Tiles.Empty & TileArray[x, y + 1] != Tiles.Empty)
+        {
+            return Tiles.T_Vert_L;
+        }
+        // T_Vert_R
+        else if (TileArray[x - 1, y] == Tiles.Empty & TileArray[x + 1, y] != Tiles.Empty 
+            & TileArray[x, y - 1] != Tiles.Empty & TileArray[x, y + 1] != Tiles.Empty)
+        {
+            return Tiles.T_Vert_R;
+        }
+        // T_Horiz_D
+        else if (TileArray[x - 1, y] != Tiles.Empty & TileArray[x + 1, y] != Tiles.Empty 
+            & TileArray[x, y - 1] != Tiles.Empty & TileArray[x, y + 1] == Tiles.Empty)
+        {
+            return Tiles.T_Horiz_D;
+        }
+        // T_Horiz_U
+        else if (TileArray[x - 1, y] != Tiles.Empty & TileArray[x + 1, y] != Tiles.Empty 
+            & TileArray[x, y - 1] == Tiles.Empty & TileArray[x, y + 1] != Tiles.Empty)
+        {
+            return Tiles.T_Horiz_U;
+        }
+        // X
+        else if (TileArray[x - 1, y] != Tiles.Empty & TileArray[x + 1, y] != Tiles.Empty 
+            & TileArray[x, y - 1] != Tiles.Empty & TileArray[x, y + 1] != Tiles.Empty)
+        {
+            return Tiles.X;
+        } 
+        // L_L_U
+        else if (TileArray[x - 1, y] != Tiles.Empty & TileArray[x, y + 1] != Tiles.Empty)
+        {
+            return Tiles.L_L_U;
+        }
+        // L_R_U
+        else if (TileArray[x + 1, y] != Tiles.Empty & TileArray[x, y + 1] != Tiles.Empty)
+        {
+            return Tiles.L_R_U;
+        }
+        // L_L_D
+        else if (TileArray[x - 1, y] != Tiles.Empty & TileArray[x, y - 1] != Tiles.Empty)
+        {
+            return Tiles.L_L_D;
+        }
+        // L_R_D
+        else if (TileArray[x + 1, y] != Tiles.Empty & TileArray[x, y - 1] != Tiles.Empty)
+        {
+            return Tiles.L_R_D;
+        }
+        // Point
+        else
+        {
+            return Tiles.Point;
+        }
+    }
+}
